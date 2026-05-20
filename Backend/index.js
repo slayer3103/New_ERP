74a6968c223321ed67972d69e16cbaf5735c06b5
slayer3103 <saurabhshendurkar3103@gmail.com>
2026-05-20 10:01:04 +0530

    add 2 new source in cors

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,11 @@ const cors = require('cors');
 
 const allowedOrigins = [
   "https://erp-system-as6ukav32-saurabhshendurkar3103-8214s-projects.vercel.app",
-  "https://erp-system-git-main-saurabhshendurkar3103-8214s-projects.vercel.app"
+  "https://erp-system-git-main-saurabhshendurkar3103-8214s-projects.vercel.app",
+  "https://new-erp-git-main-saurabhshendurkar3103-8214s-projects.vercel.app",
+  "https://new-erp-one.vercel.app",
+  "http://localhost:3000",
+  "http://localhost:5173"
 ];
 
 
